bd49d866dee9b91306517d6141e716c5c02c90f2
Kevin Earnest <kearnest23@gmail.com>
2023-08-02 17:13:37 -0400

    Changed exceptions.py.

--- project_scripts/exceptions.py
+++ project_scripts/exceptions.py
@@ -4,18 +4,4 @@ class FileException(Exception):
     """
 
     def __init__(self, message):
-        super().__init__(message)
-
-
-class ParseArgs:
-    """
-    Parses the arguments passed to main.py.  Raises 
-    exceptions as necessary.
-    """
-    def __init__(self, args: list):
-        self.args = args
-        self.length = None
-
-    def parse_files(self):
-        if len(self.args) == 1:
-            raise FileException("Please pass a plain text file to main.py.")
+        self.message = message
